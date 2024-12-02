c450c3bbcfcad216f798fbfa7fe9432f9643170b
abuah <abuahmad105116@gmail.com>
2024-12-02 11:44:39 +0600

    first

---
+++ app/src/main/java/com/example/class_2104_native_ad_template/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.class_2104_native_ad_template;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
